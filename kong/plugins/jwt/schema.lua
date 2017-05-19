0a1c9720076551658f0e7ef371ee32a33c515823
Xu Peng <xupeng@patsnap.com>
2017-05-19 13:20:09 +0800

    update jwt whitelist

--- kong/plugins/jwt/schema.lua
+++ kong/plugins/jwt/schema.lua
@@ -14,7 +14,7 @@ return {
     -- uri_param_names = {type = "array", default = {"jwt"}},
     key_claim_name = {type = "string", default = "iss"},
     slat = {type = "string", default = "zQon7#y>[)p=3267"},
-    uri_whitelist = {type= "array", default = {{method="POST", uri="/inno-user/user/v1/user/identify"}, {method="POST", uri="/inno-user/user/users/identify"}}},
+    uri_whitelist = {type= "array", default = {{method="POST", uri="/inno-user/user/v1/user/identify"}, {method="POST", uri="/inno-user/user/v1/user/sign_jwt"}, {method="POST", uri="/inno-user/user/users/identify"}, {method="POST", uri="/inno-user/user/users/sign_jwt"}}},
     secret_is_base64 = {type = "boolean", default = false},
     claims_to_verify = {type = "array", enum = {"exp", "nbf"}},
     anonymous = {type = "string", default = "", func = check_user},
